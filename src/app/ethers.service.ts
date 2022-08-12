8f53c1dfb1ead585c9dfaf06322bcb4b8e496ab2
ledevduck <devduck@tutanota.com>
2022-08-12 18:40:00 +0200

    connect service

---
+++ src/app/ethers.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, OnInit } from "@angular/core";
+import { ethers } from "ethers";
+
+@Injectable({
+  providedIn: "root",
+})
+export class EthersService implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    // Force page refreshes on network changes
+    this.provider.on("network", (newNetwork, oldNetwork) => {
+      // When a Provider makes its initial connection, it emits a "network"
+      // event with a null oldNetwork along with the newNetwork. So, if the
+      // oldNetwork exists, it represents a changing network
+      if (oldNetwork) {
+        window.location.reload();
+      }
+    });
+  }
+
+  // A Web3Provider wraps a standard Web3 provider, which is
+  // what MetaMask injects as window.ethereum into each page
+  // The "any" network will allow spontaneous network changes
+  provider = new ethers.providers.Web3Provider((window as any).ethereum, "any");
+
+  async connect() {
+    // MetaMask requires requesting permission to connect users accounts
+    await this.provider.send("eth_requestAccounts", []);
+
+    // The MetaMask plugin also allows signing transactions to
+    // send ether and pay to change state within the blockchain.
+    // For this, you need the account signer...
+    const signer = this.provider.getSigner();
+  }
+}
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,39 +1,17 @@
 import { Component, OnInit } from "@angular/core";
 
-import { ethers } from "ethers";
-
+import { EthersService } from "../ethers.service";
 @Component({
   selector: "app-top-bar",
   templateUrl: "top-bar.component.html",
   styles: [],
 })
 export class TopBarComponent implements OnInit {
-  constructor() {}
+  constructor(private ethersService: EthersService) {}
 
-  ngOnInit(): void {
-    // Force page refreshes on network changes
-    this.provider.on("network", (newNetwork, oldNetwork) => {
-      // When a Provider makes its initial connection, it emits a "network"
-      // event with a null oldNetwork along with the newNetwork. So, if the
-      // oldNetwork exists, it represents a changing network
-      if (oldNetwork) {
-        window.location.reload();
-      }
-    });
-  }
+  ngOnInit(): void {}
 
-  // A Web3Provider wraps a standard Web3 provider, which is
-  // what MetaMask injects as window.ethereum into each page
-  // The "any" network will allow spontaneous network changes
-  provider = new ethers.providers.Web3Provider((window as any).ethereum, "any");
-
-  async connect() {
-    // MetaMask requires requesting permission to connect users accounts
-    await this.provider.send("eth_requestAccounts", []);
-
-    // The MetaMask plugin also allows signing transactions to
-    // send ether and pay to change state within the blockchain.
-    // For this, you need the account signer...
-    const signer = this.provider.getSigner();
+  connect() {
+    this.ethersService.connect();
   }
 }
